70587fc1c4bb7983d16a06c00addfbc62b23bbab
ToFatToBat <kpapiernia@wp.pl>
2020-06-05 20:07:29 +0200

    Praca domowa - Driny

--- src/homework/Drink.java
+++ src/homework/Drink.java
@@ -1,50 +1,31 @@
 package homework;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 public class Drink {
     String name;
     double price;
     boolean isAlcohol;
-    String ingridient1;
-    String ingridient2;
-    String ingridient3;
-    String ingridient4;
-    String ingridient5;
-    int mlOfingridient1;
-    int mlOfingridient2;
-    int mlOfingridient3;
-    int mlOfingridient4;
-    int dddd;
 
-    public Drink(String name, double price, boolean isAlcohol, String ingridient1, String ingridient2, String ingridient3, String ingridient4, String ingridient5, int mlOfingridient1, int mlOfingridient2, int mlOfingridient3, int mlOfingridient4) {
+    Ingridient [] ingridients;
+
+    public Drink(String name, double price, boolean isAlcohol, Ingridient [] ingridients) {
+//    public Drink(String name, double price, boolean isAlcohol, List<Ingridient> ingridients) {
         this.name = name;
         this.price = price;
         this.isAlcohol = isAlcohol;
-        this.ingridient1 = ingridient1;
-        this.ingridient2 = ingridient2;
-        this.ingridient3 = ingridient3;
-        this.ingridient4 = ingridient4;
-        this.ingridient5 = ingridient5;
-        this.mlOfingridient1 = mlOfingridient1;
-        this.mlOfingridient2 = mlOfingridient2;
-        this.mlOfingridient3 = mlOfingridient3;
-        this.mlOfingridient4 = mlOfingridient4;
+        this.ingridients = ingridients;
     }
 
     @Override
     public String toString() {
-        String info =
-                "Nazwa " + name + "\n" +
-                " Cena " + price + "\n" +
-                " Czy posiada alkohol " + isAlcohol + "\n" +
-                " Składnik 1 " + ingridient1 + "\n" +
-                " Składnik 2 " + ingridient2 + "\n" +
-                " Składnik 3 " + ingridient3 + "\n" +
-                " Składnik 4 " + ingridient4 + "\n" +
-                " Składnik 5 " + ingridient5 + "\n" +
-                " Ml skladnika 1 " + mlOfingridient1 + "\n" +
-                " Ml skladnika 2 " + mlOfingridient2 + "\n" +
-                " Ml skladnika 3 " + mlOfingridient3 + "\n" +
-                " Ml skladnika 4 " + mlOfingridient4;
-        return info;
+        return "Drink{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", isAlcohol=" + isAlcohol +
+                ", ingridients=" + Arrays.toString(ingridients) +
+                '}';
     }
 }
--- src/homework/DrinkBar.java
+++ src/homework/DrinkBar.java
@@ -2,9 +2,17 @@ package homework;
 
 public class DrinkBar {
     public static void main(String[] args) {
-       Drink cosmopolitan = new Drink("Cosmopolitan", 24,true, "orangeVodka", "orangeLiqueur", "cranberryJuice", "limeJuice", "limePeel", 60, 15,40, 10);
+       // Drink cosmopolitan = new Drink("Cosmopolitan", 24,true, "orangeVodka", "orangeLiqueur", "cranberryJuice", "limeJuice", "limePeel", 60, 15,40, 10);
 
-        System.out.println(cosmopolitan.toString());
+        Ingridient [] ingridients = new Ingridient[1];
+        ingridients[0] = new Ingridient("woda", 100);
+
+        Drink drink = new Drink("woda",20, true,ingridients);
+
+
+
+
+        System.out.println(drink.toString());
 
     }
 }
